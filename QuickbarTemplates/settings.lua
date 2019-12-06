e21986d01472bca3c285ea96ab917cab2d435fcb
raiguard <caleb.heuer@gmail.com>
2019-12-05 19:28:41 -0700

    Quickbar Templates skeleton

---
+++ QuickbarTemplates/settings.lua
@@ -0,0 +1,10 @@
+data:extend{
+    {
+        type = 'string-setting',
+        name = 'qt-default-template',
+        setting_type = 'runtime-per-user',
+        allow_blank = true,
+        default_value = '',
+        order = 'a'
+    }
+}
