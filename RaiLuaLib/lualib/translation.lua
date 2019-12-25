8fdcfec6707e3a86dd6b5efba2873bfee3da52b1
raiguard <caleb.heuer@gmail.com>
2019-12-25 01:09:12 -0700

    Event handling, fix iteration count math

--- RaiLuaLib/lualib/translation.lua
+++ RaiLuaLib/lualib/translation.lua
@@ -4,18 +4,21 @@
 
 -- DOCUMENTATION: https://github.com/raiguard/Factorio-SmallMods/wiki/Translation-Library-Documentation
 
---[[
-  player.request_translation(localised_string)
-  -> event.on_string_translated(e)
-]]
+-- dependencies
+local event = require('lualib/event')
+local util = require('__core__/lualib/util')
 
+-- locals
 local string_gsub = string.gsub
-local string_lower = string.lower
+local math_floor = math.floor
+
+-- -----------------------------------------------------------------------------
+
+local registered_mod_count = 1
 
 local translation = {}
-local registered_mod_count = 1
-translation.start_event = script.generate_event_name()
-translation.finish_event = script.generate_event_name()
+translation.start_event = event.generate_id('translation_start')
+translation.finish_event = event.generate_id('translation_finish')
 
 -- converts a localised string into a format readable by the API
 -- basically just spits out the table in string form
@@ -35,32 +38,61 @@ end
 -- translate 100 entries per tick
 local function translate_batch(e)
   local __translation = global.__translation
-  if __translation and __translation.event_registered then -- if we should be doing something
-    local iterations = 100 / __translation.dictionary_count / registered_mod_count
-    for pi,pt in pairs(__translation.players) do -- for each player that is doing a translation
-      for name,t in pairs(pt) do -- for each dictionary that they're translating
-        local next_index = t.next_index
-        local request_translation = t.request_translation
-        local strings = t.strings
-        local strings_len = t.strings_len
-        for i=next_index,next_index+iterations do
-          if i <= strings_len then
-            request_translation(strings[i])
+  local iterations = math_floor(100 / __translation.dictionary_count / registered_mod_count)
+  for _,pt in pairs(__translation.players) do -- for each player that is doing a translation
+    for _,t in pairs(pt) do -- for each dictionary that they're translating
+      local next_index = t.next_index
+      local request_translation = t.request_translation
+      local strings = t.strings
+      local strings_len = t.strings_len
+      for i=next_index,next_index+iterations do
+        if i <= strings_len then
+          if strings[i] == nil then
+            local breakpoint
           end
+          request_translation(strings[i])
         end
-        t.next_index = next_index + iterations
       end
+      t.next_index = next_index + iterations
     end
   end
 end
 
+local function parse_translation_result(e)
+  local __translation = global.__translation
+  local player_translation = __translation.players[e.player_index]
+  local serialised = serialise_localised_string(e.localised_string)
+  for name,t in pairs(player_translation) do
+    local value = t.data[serialised]
+    if value then
+      t.result[e.result] = value
+      t.data[serialised] = nil
+      if table_size(t.data) == 0 then -- this dictionary has completed translation
+        local result = t.result
+        player_translation[name] = nil
+        __translation.dictionary_count = __translation.dictionary_count - 1
+        if table_size(player_translation) == 0 then
+          __translation.players[e.player_index] = nil
+          if table_size(__translation.players) == 0 then
+            event.deregister(defines.events.on_tick, translate_batch, {name='translation_translate_batch'})
+            event.deregister(defines.events.on_string_translated, parse_translation_result, {name='translation_parse_result'})
+            global.__translation = nil
+          end
+        end
+        event.raise(translation.finish_event, {player_index=e.player_index, dictionary_name=name, dictionary=t.result})
+      end
+      return
+    end
+  end
+end
+
+translation.serialise_localised_string = serialise_localised_string
+
 function translation.start(player, dictionary_name, data, strings)
   if not global.__translation then
     global.__translation = {
-      event_registered = false,
       dictionary_count = 0,
-      players = {},
-      player_count = 0
+      players = {}
     }
   end
   local __translation = global.__translation
@@ -69,7 +101,7 @@ function translation.start(player, dictionary_name, data, strings)
   if player_translation[dictionary_name] then error('Already translating dictionary: '..dictionary_name) end
   player_translation[dictionary_name] = {
     -- tables
-    data = data,
+    data = table.deepcopy(data), -- this table gets destroyed as it is translated, so deepcopy it
     strings = strings,
     -- iteration
     next_index = 1,
@@ -79,37 +111,14 @@ function translation.start(player, dictionary_name, data, strings)
     -- output
     result = {}
   }
-  __translation.event_registered = true
   __translation.dictionary_count = __translation.dictionary_count + 1
-end
-
-function translation.on_string_translated_event(e)
-  local profiler = game.create_profiler()
-  local __translation = global.__translation
-  local player_translation = __translation.players[e.player_index]
-  local serialised = serialise_localised_string(e.localised_string)
-  for name,t in pairs(player_translation) do
-    local value = t.data[serialised]
-    if value then
-      t.result[e.result] = value
-      t.data[serialised] = nil
-      if table_size(t.data) == 0 then
-        local result = t.result
-        player_translation[name] = nil
-        if table_size(player_translation) == 0 then
-          __translation.players[e.player_index] = nil
-          if table_size(__translation.players) == 0 then
-            __translation.event_registered = false
-          end
-        end
-        return result
-      end
-      return
-    end
+  if not event.is_registered('translation_translate_batch') then
+    event.on_tick(translate_batch, {name='translation_translate_batch'})
+    event.on_string_translated(parse_translation_result, {name='translation_parse_result'})
   end
+  event.raise(translation.start_event, {player_index=player.index, dictionary_name=dictionary_name})
 end
 
-translation.serialise_localised_string = serialise_localised_string
-translation.on_tick_event = translate_batch
+
 
 return translation
--- RaiLuaLib/tests/translation/control.lua
+++ RaiLuaLib/tests/translation/control.lua
@@ -3,33 +3,66 @@
 
 local event = require('lualib/event')
 local translation = require('lualib/translation')
+local util = require('lualib/util')
 
 local serialise_localised_string = translation.serialise_localised_string
 
+-- build LOTS of data to really stress the translation engine
 local function build_data()
-  local data = {}
-  local strings = {}
-  for name,prototype in pairs(game.recipe_prototypes) do
-    data[serialise_localised_string(prototype.localised_name)] = name
-    table.insert(strings, prototype.localised_name)
+  local build = {}
+  local function generic_setup(key)
+    local data = {}
+    local strings = {}
+    local strings_len = 0
+    for name,prototype in pairs(game[key..'_prototypes']) do
+      data[serialise_localised_string(prototype.localised_name)] = name
+      strings_len = strings_len + 1
+      strings[strings_len] = prototype.localised_name
+    end
+    return {data=data, strings=strings}
   end
-  global.data = data
-  global.strings = strings
+  build.achievement = generic_setup('achievement')
+  build.entity = generic_setup('entity')
+  build.equipment = generic_setup('equipment')
+  build.fluid = generic_setup('fluid')
+  build.item = generic_setup('item')
+  build.recipe = generic_setup('recipe')
+  build.technology = generic_setup('technology')
+  build.tile = generic_setup('tile')
+  global.__build = build
+end
+
+local function translate_whole(player)
+  for name,t in pairs(global.__build) do
+    translation.start(player, name, t.data, t.strings)
+  end
+end
+
+local function first_tick(e)
+  for _,player in ipairs(game.connected_players) do
+    translate_whole(player)
+  end
+  event.deregister(defines.events.on_tick, first_tick)
 end
 
 event.on_init(function()
   build_data()
+  if #game.connected_players > 0 then
+    event.on_tick(first_tick)
+  end
 end)
 
 event.on_configuration_changed(function()
   build_data()
+  if #game.connected_players > 0 then
+    event.on_tick(first_tick)
+  end
 end)
 
-event.on_string_translated(translation.on_string_translated_event)
+event.on_player_joined_game(function(e)
+  translate_whole(game.get_player(e.player_index))
+end)
 
-event.on_tick(function(e)
-  if e.tick == 1 then
-    translation.start(game.get_player(1), 'recipe', global.data, global.strings)
-  end
-  translation.on_tick_event(e)
+event.register(translation.finish_event, function(e)
+  game.print('finished translation of dictionary: '..e.dictionary_name)
 end)
