0e30806c5c2dbac57063dba67218a0b2e73a6c0b
raiguard <caleb.heuer@gmail.com>
2019-12-31 10:27:21 -0700

    Failed experiment

--- RaiLuaLib/tests/sandbox/control.lua
+++ RaiLuaLib/tests/sandbox/control.lua
@@ -19,13 +19,14 @@ end)
 
 script.on_event({'sandbox-zoom-in', 'sandbox-zoom-out', 'sandbox-zoom-in-extended', 'sandbox-zoom-out-extended'}, function(e)
   local player = game.get_player(e.player_index)
-  if player.render_mode == defines.render_mode.game and player.controller_type ~= defines.controllers.editor then
+  if player.render_mode == defines.render_mode.game then
     local player_table = global.players[e.player_index]
     local extended = e.input_name:find('extended') and true or false
+    local in_map_editor = player.controller_type == defines.controllers.editor
     if e.input_name:find('in') then
-      player_table.zoom = math.min(player_table.zoom * delta, (extended and 40 or 3))
+      player_table.zoom = math.min(player_table.zoom * delta, ((extended or in_map_editor) and 40 or 3))
     else
-      player_table.zoom = math.max(player_table.zoom / delta, (extended and 0.1 or 0.5))
+      player_table.zoom = math.max(player_table.zoom / delta, ((extended or in_map_editor) and 0.1 or 0.5))
     end
     player.zoom = player_table.zoom
   end
