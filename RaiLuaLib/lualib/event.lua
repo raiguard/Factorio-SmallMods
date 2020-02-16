2b0dc0a866f93e0c0825ccd9c139eead974dfd7d
raiguard <caleb.heuer@gmail.com>
2020-02-16 13:24:00 -0700

    RLL: Add copyright info to the event module

--- RaiLuaLib/lualib/event.lua
+++ RaiLuaLib/lualib/event.lua
@@ -1,7 +1,15 @@
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- RAILUALIB EVENT MODULE
+-- Multi-handler registration, conditional event handling, and GUI event filtering.
 
--- DOCUMENTATION: https://github.com/raiguard/SmallFactorioMods/wiki/Event-Module-Documentation
+-- Copyright (c) 2020 raiguard - https://github.com/raiguard
+-- Permission is hereby granted, free of charge, to those obtaining this software or a portion thereof, to copy the contents of this software into their own
+-- Factorio mod, and modify it to suit their needs. This is permissed under the condition that this notice and copyright information, as well as the link to
+-- the documentation, are not omitted, and that any changes from the original are documented.
+
+-- DOCUMENTATION: https://github.com/raiguard/Factorio-SmallMods/wiki/Event-Module-Documentation
+
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local util = require('__core__/lualib/util')
 
