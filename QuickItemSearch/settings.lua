e4c18be6e23c02b8478053bccb063c6dcaffdefd
raiguard <caleb.heuer@gmail.com>
2020-04-20 21:08:12 -0600

    QIS: Remove location and dimensions settings

--- QuickItemSearch/settings.lua
+++ QuickItemSearch/settings.lua
@@ -40,37 +40,5 @@ data:extend{
     setting_type = 'runtime-per-user',
     default_value = true,
     order = 'af'
-  },
-  {
-    type = 'string-setting',
-    name = 'qis-default-location',
-    setting_type = 'runtime-per-user',
-    allowed_values = {'mod gui', 'top', 'bottom', 'center'},
-    default_value = 'center',
-    order = 'ba'
-  },
-  {
-    type = 'string-setting',
-    name = 'qis-editor-location',
-    setting_type = 'runtime-per-user',
-    allowed_values = {'mod gui', 'top', 'bottom', 'center'},
-    default_value = 'center',
-    order = 'bb'
-  },
-  {
-    type = 'int-setting',
-    name = 'qis-column-count',
-    setting_type = 'runtime-per-user',
-    default_value = 5,
-    minimum_value = 5,
-    order = 'ca'
-  },
-  {
-    type = 'int-setting',
-    name = 'qis-row-count',
-    setting_type = 'runtime-per-user',
-    default_value = 4,
-    minimum_value = 1,
-    order = 'cb'
   }
 }
