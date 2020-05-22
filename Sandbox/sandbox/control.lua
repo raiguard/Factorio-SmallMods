e8d40843bc5f77caee7269305eb4ec6aa61a944e
raiguard <caleb.heuer@gmail.com>
2020-05-21 20:27:13 -0600

    Sandbox: Lab generation test (0.18.27)

--- Sandbox/sandbox/control.lua
+++ Sandbox/sandbox/control.lua
@@ -1,6 +1,20 @@
-local event = require("__flib__.control.event")
+local event = require("__flib__.event")
 
 event.on_init(function()
-  local handler = event.get_handler(defines.events.on_gui_click)
-  local breakpoint
+  local nauvis = game.surfaces.nauvis
+  nauvis.generate_with_lab_tiles = true
+  nauvis.clear(true)
+  local read = nauvis.generate_with_lab_tiles
+  log("STATE: "..tostring(read))
+end)
+
+event.on_player_created(function(e)
+  local player = game.get_player(e.player_index)
+  player.gui.top.add{type="checkbox", name="sandbox", caption="Toggle", state=true}
+end)
+
+event.on_gui_checked_state_changed(function(e)
+  if e.element.name == "sandbox" then
+    game.surfaces.nauvis.generate_with_lab_tiles = e.element.state
+  end
 end)
