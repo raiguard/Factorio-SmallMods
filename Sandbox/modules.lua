453d4e752a59ca5ba4216386dc824197c309fe85
raiguard <caleb.heuer@gmail.com>
2020-06-14 22:48:44 -0600

    Sandbox: more colors testing

--- Sandbox/modules.lua
+++ Sandbox/modules.lua
@@ -8,5 +8,5 @@ return {
   -- "flib-tests.translation-desync-standalone",
   -- "recipe-book-mockup",
   -- "resource-tracker",
-  -- "sandbox"
+  "sandbox"
 }
---
+++ Sandbox/sandbox/data.lua
@@ -0,0 +1,7 @@
+local styles = data.raw["gui-style"].default
+
+styles.green_slot = styles.flib_slot_green
+styles.blue_slot = styles.flib_slot_blue
+styles.yellow_slot = styles.flib_slot_yellow
+styles.red_slot = styles.flib_slot_red
+styles.closed_inventory_slot = styles.flib_slot_red
