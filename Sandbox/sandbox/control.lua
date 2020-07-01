d58089137a94e3e7b37095d0618919317e2faacc
raiguard <caleb.heuer@gmail.com>
2020-07-01 17:34:07 -0600

    Sandbox: update

--- Sandbox/sandbox/control.lua
+++ Sandbox/sandbox/control.lua
@@ -1,27 +1,25 @@
 local event = require("__flib__.event")
-local gui = require("__flib__.gui")
 
-event.on_init(function()
-  gui.init()
-  gui.build_lookup_tables()
-end)
-
-event.on_load(function()
-  gui.build_lookup_tables()
-end)
-
-event.on_player_created(function(e)
+event.on_selected_entity_changed(function(e)
+  if e.last_entity then
+    rendering.destroy(global.module_bonus)
+    rendering.destroy(global.lab_bonus)
+  end
   local player = game.get_player(e.player_index)
-  local display_scale = player.display_scale
-
-  local root = player.gui.goal
-  root.clear()
-
-  gui.build(root, {
-    {type="frame", children={
-      {type="flow", style_mods={width=math.max(384 / display_scale, 300), horizontal_align="center"}, direction="vertical", children={
-
-      }}
-    }}
-  })
+  local entity = player.selected
+  if entity then
+    global.module_bonus = rendering.draw_text{
+      text = entity.speed_bonus,
+      surface = player.surface,
+      target = entity,
+      color = {r=1, g=1, b=1}
+    }
+    global.lab_bonus = rendering.draw_text{
+      text = player.force.laboratory_speed_modifier,
+      surface = player.surface,
+      target = entity,
+      target_offset = {0, 0.5},
+      color = {r=1, g=1, b=1}
+    }
+  end
 end)
