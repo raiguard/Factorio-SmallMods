5cdbc1916bc20da56922ad5966ec5fa463c02b42
raiguard <caleb.heuer@gmail.com>
2020-07-13 23:26:48 -0600

    Sandbox: example for reika

--- Sandbox/sandbox/control.lua
+++ Sandbox/sandbox/control.lua
@@ -1,25 +1,28 @@
 local event = require("__flib__.event")
+local gui = require("__flib__.gui")
+local mod_gui = require("__core__.lualib.mod-gui")
 
-event.on_selected_entity_changed(function(e)
-  if e.last_entity then
-    rendering.destroy(global.module_bonus)
-    rendering.destroy(global.lab_bonus)
-  end
+event.on_player_created(function(e)
   local player = game.get_player(e.player_index)
-  local entity = player.selected
-  if entity then
-    global.module_bonus = rendering.draw_text{
-      text = entity.speed_bonus,
-      surface = player.surface,
-      target = entity,
-      color = {r=1, g=1, b=1}
-    }
-    global.lab_bonus = rendering.draw_text{
-      text = player.force.laboratory_speed_modifier,
-      surface = player.surface,
-      target = entity,
-      target_offset = {0, 0.5},
-      color = {r=1, g=1, b=1}
-    }
-  end
+  local frame_flow = mod_gui.get_frame_flow(player)
+  gui.build(frame_flow, {
+    {type="frame", style_mods={top_padding=12}, children={
+      {type="frame", style="inside_shallow_frame_with_padding", direction="vertical", children={
+        {type="table", style="bordered_table", column_count=1, children={
+          {type="flow", style_mods={vertical_align="center"}, children={
+            {type="label", style="caption_label", style_mods={width=100}, caption="[item=coal] 3600"},
+            {type="sprite-button", style="tool_button", sprite="utility/export"}
+          }},
+          {type="flow", style_mods={vertical_align="center"}, children={
+            {type="label", style="caption_label", style_mods={width=100}, caption="[item=coal] 3600"},
+            {type="sprite-button", style="tool_button", sprite="utility/export"}
+          }},
+          {type="flow", style_mods={vertical_align="center"}, children={
+            {type="label", style="caption_label", style_mods={width=100}, caption="[item=coal] 3600"},
+            {type="sprite-button", style="tool_button", sprite="utility/export"}
+          }}
+        }}
+      }}
+    }}
+  })
 end)
