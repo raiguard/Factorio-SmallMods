a7e48b63c750c3a65c13065da7cedaa78a83da5c
raiguard <caleb.heuer@gmail.com>
2020-07-26 12:36:01 -0600

    Sandbox: flib tests

--- Sandbox/modules.lua
+++ Sandbox/modules.lua
@@ -2,6 +2,7 @@ return {
   -- "flib-tests.event"
   -- "flib-tests.gui-demo"
   -- "flib-tests.gui",
+  "flib-tests.gui-remove-player-filters",
   -- "flib-tests.migration",
   -- "flib-tests.slot-styles",
   -- "flib-tests.translation",
