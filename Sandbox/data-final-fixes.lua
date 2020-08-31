d674afa99cbc05a13322c23a69f6e291d503d7ed
raiguard <caleb.heuer@gmail.com>
2020-08-31 11:31:02 -0600

    Sandbox: support all data stages, update

--- Sandbox/data-final-fixes.lua
+++ Sandbox/data-final-fixes.lua
@@ -1,4 +1,4 @@
 local modules = require("modules")
 for _, module in pairs(modules) do
-  pcall(require, module..".data")
+  pcall(require, module..".data-final-fixes")
 end
---
+++ Sandbox/sandbox/data-final-fixes.lua
@@ -0,0 +1,26 @@
+-- data.raw["rocket-silo"]["rocket-silo"].fixed_recipe = nil
+-- data.raw["rocket-silo"]["rocket-silo"].rocket_result_inventory_size = 5
+-- data.raw["rocket-silo-rocket"]["rocket-silo-rocket"].inventory_size = 5
+-- data.raw["tool"]["space-science-pack"].rocket_launch_product = nil
+-- data.raw["tool"]["space-science-pack"].rocket_launch_products = {
+--   {"raw-fish", 100},
+--   {"uranium-235", 50}
+-- }
+-- data.raw["recipe"]["rocket-part"].hidden = false
+
+-- data:extend{
+--   {
+--     type = "recipe",
+--     name = "rocket-part-alt",
+--     energy_required = 1,
+--     enabled = true,
+--     hidden = false,
+--     category = "rocket-building",
+--     ingredients = {
+--       {"raw-fish", 10}
+--     },
+--     result= "grenade"
+--   }
+-- }
+
+-- data.raw["assembling-machine"]["assembling-machine-1"].fixed_recipe = "automation-science-pack"
