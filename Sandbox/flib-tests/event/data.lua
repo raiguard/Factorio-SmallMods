de28bd3332bd0cef7bd547567cdc3c10a657a0c9
raiguard <caleb.heuer@gmail.com>
2020-09-12 22:23:41 -0600

    Sandbox: more flib testing

---
+++ Sandbox/flib-tests/event/data.lua
@@ -0,0 +1,12 @@
+data:extend{
+  {
+    type = "custom-input",
+    name = "my-custom-input",
+    key_sequence = "CONTROL + ENTER"
+  },
+  {
+    type = "custom-input",
+    name = "my-other-custom-input",
+    key_sequence = "CONTROL + ENTER"
+  },
+}
--- Sandbox/flib-tests/math/control.lua
+++ Sandbox/flib-tests/math/control.lua
@@ -2,15 +2,8 @@ local math = require("__flib__.math")
 
 local num = 3.14159265358
 
-local round = math.round(num)
-local round_3 = math.round(num, 3)
-
-local ceil = math.round(num, 0, "ceil")
-local ceil_3 = math.round(num, 3, "ceil")
-
-local floor = math.round(num, 0, "floor")
-local floor_3 = math.round(num, 3, "floor")
-
-local mean = math.mean{1, 2, 3, 4, 5, 100}
-
-local breakpoint
+for _ = 1, 1000000 do
+  math.round(num)
+  math.round_to(num, 0)
+  math.round_to(num, 5)
+end
