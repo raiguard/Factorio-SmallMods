57fe61b3160ecfa6e854f5f5d83db00f7b7a2e3f
raiguard <caleb.heuer@gmail.com>
2020-10-07 17:55:48 -0600

    Sandbox: GUI3 TodoMVC example

--- Sandbox/modules/flib-tests/gui3/control.lua
+++ Sandbox/modules/flib-tests/gui3/control.lua
@@ -1,7 +1,8 @@
 local event = require("__flib__.event")
 local gui = require("__flib__.gui3")
 
-local counter = require("modules.flib-tests.gui3.counter")
+local counter_gui = require("modules.flib-tests.gui3.counter")
+local todo_gui = require("modules.flib-tests.gui3.todo")
 
 event.on_init(function()
   gui.init()
@@ -14,14 +15,19 @@ end)
 
 event.on_player_created(function(e)
   local player = game.get_player(e.player_index)
-  local Counter = counter:new(player.gui.screen)
-  global.players[e.player_index] = Counter
+  local Counter = counter_gui:new(player.gui.screen)
+  local Todo = todo_gui:new(player.gui.screen)
+  global.players[e.player_index] = {
+    counter = Counter,
+    todo = Todo
+  }
 end)
 
 event.on_player_removed(function(e)
-  local Counter = global.players[e.player_index]
-  if Counter then
-    Counter:destroy()
+  local player_table = global.players[e.player_index]
+  if player_table then
+    player_table.counter:destroy()
+    player_table.todo:destroy()
     global.players[e.player_index] = nil
   end
 end)
