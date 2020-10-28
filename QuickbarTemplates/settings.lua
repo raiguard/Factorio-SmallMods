84b82a25dfb83c7ed55f74f42d1ec65f5d6000c6
raiguard <caleb.heuer@gmail.com>
2020-10-28 14:27:14 -0600

    QuickbarTemplates: fix settings.lua indentation

--- QuickbarTemplates/settings.lua
+++ QuickbarTemplates/settings.lua
@@ -1,10 +1,10 @@
 data:extend{
-    {
-        type = "string-setting",
-        name = "qt-default-template",
-        setting_type = "runtime-per-user",
-        allow_blank = true,
-        default_value = "",
-        order = "a"
-    }
+  {
+    type = "string-setting",
+    name = "qt-default-template",
+    setting_type = "runtime-per-user",
+    allow_blank = true,
+    default_value = "",
+    order = "a"
+  }
 }
