514a8b673e0037a64555adf96cde0a5b4388e032
raiguard <caleb.heuer@gmail.com>
2020-11-23 16:54:27 -0700

    CCP: update to Factorio 1.1

--- ColorCodedPlanners/data-updates.lua
+++ ColorCodedPlanners/data-updates.lua
@@ -1,38 +1,28 @@
 local changes = {
   ["blueprint"] = {
     ["blueprint"] = {
-      selection_color = {r=0, g=0.5, b=1},
-      alt_selection_color = {r=0, g=1, b=1},
       selection_cursor_box_type = "electricity",
       alt_selection_cursor_box_type = "electricity"
     }
   },
   ["copy-paste-tool"] = {
     ["cut-paste-tool"] = {
-      selection_color = {r=1, g=0, b=0},
-      alt_selection_color = {r=1, g=0, b=0.75},
       selection_cursor_box_type = "not-allowed",
       alt_selection_cursor_box_type = "not-allowed"
     },
     ["copy-paste-tool"] = {
-      selection_color = {r=0, g=0.5, b=1},
-      alt_selection_color = {r=0, g=1, b=1},
       selection_cursor_box_type = "electricity",
       alt_selection_cursor_box_type = "electricity"
     }
   },
   ["deconstruction-item"] = {
     ["deconstruction-planner"] = {
-      selection_color = {r=1, g=0, b=0},
-      alt_selection_color = {r=1, g=0, b=0.75},
       selection_cursor_box_type = "not-allowed",
       alt_selection_cursor_box_type = "not-allowed"
     }
   },
   ["upgrade-item"] = {
     ["upgrade-planner"] = {
-      selection_color = {r=0, g=1, b=0},
-      alt_selection_color = {r=0.75, g=1, b=0},
       selection_cursor_box_type = "copy",
       alt_selection_cursor_box_type = "copy"
     }
