2ee41ef3630d6c4d5c2242473f77ae5db2c733db
raiguard <caleb.heuer@gmail.com>
2021-03-05 02:46:13 -0700

    Better alert arrows

---
+++ BetterAlertArrows/settings.lua
@@ -0,0 +1,16 @@
+data:extend{
+  {
+    type = "double-setting",
+    name = "baa-arrow-scale",
+    setting_type = "startup",
+    minimum_value = 0,
+    maximum_value = 1,
+    default_value = 0.2
+  },
+  {
+    type = "string-setting",
+    name = "baa-arrow-tint",
+    setting_type = "startup",
+    default_value = "FF9F1C"
+  }
+}
