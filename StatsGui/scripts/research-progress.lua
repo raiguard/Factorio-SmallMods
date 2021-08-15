9198e8b39d9d5148b95a536e40ba11e9ed5eefeb
raiguard <caleb.heuer@gmail.com>
2021-08-15 13:42:16 -0600

    Show infinity symbol if researching has stopped

--- StatsGui/scripts/research-progress.lua
+++ StatsGui/scripts/research-progress.lua
@@ -2,7 +2,7 @@ local misc = require("__flib__.misc")
 
 local constants = require("constants")
 
--- code based on "improved research queue" by sonaxaton
+-- Code based on "improved research queue" by sonaxaton
 -- https://github.com/dbeckwith/factorio-research-queue/blob/af6404ab696502a86eec40a4baf7fe00d0c714c7/control.lua#L316
 return function()
   local strings = {}
@@ -10,7 +10,7 @@ return function()
   for _, force in pairs(game.forces) do
     local tech = force.current_research
     if tech then
-      -- retrieve or create progress samples table
+      -- Retrieve or create progress samples table
       local progress_samples = global.research_progress_samples[force.index]
       if not progress_samples then
         progress_samples = {}
@@ -22,29 +22,34 @@ return function()
       end
 
       local estimated_ticks = 0
+      local num_samples = 0
       if #progress_samples > 1 then
-        local num_samples = 0
         for i = 2, #progress_samples do
           local previous_sample = progress_samples[i - 1]
           local current_sample = progress_samples[i]
           if previous_sample.tech == current_sample.tech then
-            -- how much the progress increased per tick
+            -- How much the progress increased per tick
             local speed = (current_sample.progress - previous_sample.progress) / 60
-            -- don't add if the speed is negative for whatever reason
+            -- Don't add if the speed is negative for whatever reason
             if speed > 0 then
-              -- how many ticks left until the research is finished
+              -- How many ticks left until the research is finished
               estimated_ticks = estimated_ticks + ((1 - current_sample.progress) / speed)
               num_samples = num_samples + 1
             end
           end
         end
-        -- rolling average
+        -- Rolling average
         if num_samples > 0 then
           estimated_ticks = estimated_ticks / num_samples
         end
       end
 
-      strings[force.index] = misc.ticks_to_timestring(estimated_ticks)
+      if num_samples > 0 then
+        strings[force.index] = misc.ticks_to_timestring(estimated_ticks)
+      else
+        strings[force.index] = "∞"
+      end
+
     end
   end
 
